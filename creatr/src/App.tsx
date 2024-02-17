f2355dcff20024f37cc36650c4cae1d2b513e7ac
Ian Bravyn <ianbravynsa@gmail.com>
2024-02-17 19:05:47 +0300

    updated input text and minor styles

--- creatr/src/App.tsx
+++ creatr/src/App.tsx
@@ -1,26 +1,26 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+<div className="app-container">
+  <form className='note-form'>
+    <input placeholder='Title' required/>
+    <textarea placeholder='content' rows = {10} required />
+    <button type='submit'>Add progress note</button>
+  </form>
+  <div className="notes-grid">
+    <div className="note-item">
+      <div className="notes-header">
+        <button>X</button>
+      </div>
+      <h2>Note Title</h2>
+      <p>Note Content</p>
     </div>
-  );
-}
+  </div>
+</div>
+
+  )
+  };
 
 export default App;
