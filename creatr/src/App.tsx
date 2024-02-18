6f44f13b767aa28ce0062f425a7915943aecb43a
Ian Bravyn <ianbravynsa@gmail.com>
2024-02-18 13:03:31 +0300

    changed favicon, added dummy data for testing

--- creatr/src/App.tsx
+++ creatr/src/App.tsx
@@ -1,7 +1,47 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  type Note = {
+    id: number;
+    title: string;
+    content: string;
+  }
+  
+  const [notes, setNotes] = useState<Note[]> ([
+    {
+      id: 1,
+      title: "Test note 1",
+      content: "This is a test note"
+    },
+    {
+      id: 2,
+      title: "Test note 2",
+      content: "This is a test note"
+    },
+    {
+      id: 3,
+      title: "Test note 3",
+      content: "This is a test note"
+    },
+    {
+      id: 4,
+      title: "Test note 4",
+      content: "This is a test note"
+    },
+    {
+      id: 5,
+      title: "Test note 5",
+      content: "This is a test note"
+    },
+    {
+      id: 6,
+      title: "Test note 6",
+      content: "This is a test note"
+    }
+    
+
+  ])
   return (
 <div className="app-container">
   <form className='note-form'>
